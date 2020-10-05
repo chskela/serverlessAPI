caa9681b9ca1fe9e0117a31e1b4d85f21947980a
Aleksey Chistyakov <chskela@gmail.com>
2020-10-05 22:12:27 +0300

    add create-order

--- api.js
+++ api.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const API = require('claudia-api-builder');
-const getPizzas = require('./handlers/get-pizzas.js');
 
 const api = new API();
 
-api.get('/', () => 'Welcome to Pizza API' );
+const getPizzas = require('./handlers/get-pizzas');
+const createOrder = require('./handlers/create-order');
+
+api.get('/', () => 'Welcome to Pizza API');
 
 api.get('/pizzas', () => getPizzas());
 
-api.get('/pizzas/{id}', request => getPizzas(request.pathParam.id), {
-  error: 404
-});
+api.get('/pizzas/{id}',
+  request => getPizzas(request.pathParams.id),
+  {
+    error: 404
+  });
+
+api.post('/orders',
+  request => createOrder(request.body),
+  {
+    success: 201,
+    error: 400
+  });
 
 module.exports = api;
+
+// curl -i -H "Content-Type: application/json" -X POST -d '{"pizzaId":1,"address":"221B Baker Street"}' https://pw7ortv99l.execute-api.eu-central-1.amazonaws.com/latest/orders
+
