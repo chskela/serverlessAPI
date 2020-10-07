493d2cd736be8a656b6d5a0f1dece2efba8877ef
chskela <chskela@mail.ru>
2020-10-07 21:54:50 +0300

    add deleteOrder and updateOrder

--- api.js
+++ api.js
@@ -6,6 +6,8 @@ const api = new API();
 
 const getPizzas = require('./handlers/get-pizzas');
 const createOrder = require('./handlers/create-order');
+const updateOrder = require('./handlers/update-order');
+const deleteOrder = require('./handlers/delete-order');
 
 api.get('/', () => 'Welcome to Pizza API');
 
@@ -24,6 +26,18 @@ api.post('/orders',
     error: 400
   });
 
+api.put('/orders/{id}',
+  request => updateOrder(request.pathParams.id, request.body),
+  {
+    error: 400
+  });
+  
+api.delete('/orders/{id}',
+  request => deleteOrder(request.pathParams.id),
+  {
+    error: 400
+  });
+  
 module.exports = api;
 
 // curl -i -H "Content-Type: application/json" -X POST -d '{"pizzaId":1,"address":"221B Baker Street"}' https://pw7ortv99l.execute-api.eu-central-1.amazonaws.com/latest/orders
---
+++ handlers/delete-order.js
@@ -0,0 +1,9 @@
+function deleteOrder(orderId) {
+  if (!orderId) {
+    throw new Error('Order ID is required for deleting the order');
+  }
+
+  return {};
+}
+
+module.exports = deleteOrder;
